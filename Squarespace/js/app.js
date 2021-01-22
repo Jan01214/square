6577075022ae78efbbf77a6df7a3885ffd9354ab
Jan01214 <71278204+Jan01214@users.noreply.github.com>
2021-01-22 14:05:09 +0100

    version 1.0
    Only responsive header

---
+++ Squarespace/js/app.js
@@ -0,0 +1,17 @@
+// const burger = document.querySelector('.burger');
+// const menu = document.querySelector('.header__nav');
+
+// burger.forEach(function(elem){
+// 	elem.addEventListener('click', changeClass)
+// })
+
+
+// function changeClass(){
+// 	burger.forEach(function(elem){
+// 		elem.classList.toggle('active');
+// 	})
+// }
+
+
+
+
